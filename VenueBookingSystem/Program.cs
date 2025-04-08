68bedcc912c6aa754ef458b527452486bcdbf09d
Uthandile Nkosi <uthandilenkosi@Uthandiles-MBP.lan>
2025-04-08 20:09:46 +0200

    Core updates: Program.cs and csproj config

--- VenueBookingSystem/Program.cs
+++ VenueBookingSystem/Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using VenueBookingSystem.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+//Register databse context
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
